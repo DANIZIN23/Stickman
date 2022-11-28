8728b229e68164d2344d7736dd6e3fb11bde5f98
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-28 15:20:45 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -75,8 +75,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if cpp
-		initialState = TitleState;
+		
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
