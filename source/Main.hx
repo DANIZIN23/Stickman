54fad928b98387da6ab36f1acecd6d8d7d643c02
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-28 14:31:59 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -78,9 +78,9 @@ class Main extends Sprite
 		#if cpp
 		initialState = TitleState;
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#else
+		
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-		#end
+		
 		addChild(game);
 		
 		#if !mobile
